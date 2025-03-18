4c524e921615aa6787b44a89d422f91ee0652fcd
hemanth-ks97 <hemanth.ks97@gmail.com>
2025-03-18 18:27:11 -0500

    Added Bottom Up solution with optimization to 1D array

--- problem1.py
+++ problem1.py
@@ -6,7 +6,7 @@
 
 # Your code here along with comments explaining your approach
 import math
-class Solution:
+class TopDownSolution:
     def coinChange(self, coins: List[int], amount: int) -> int:
         cache = {}
         
@@ -50,3 +50,30 @@ class Solution:
         res = search(0,amount)
 
         return res if res != math.inf else -1
+
+# Time Complexity : O(amount*len(coins))
+# Space Complexity : O(amount)
+# Did this code successfully run on Leetcode : YES
+# Any problem you faced while coding this : NO
+
+class BottomUpSolution:
+        def coinChange(self, coins: List[int], amount: int) -> int:
+            #dp = [[float('inf')] * (amount + 1) for i in range(len(coins) + 1)] --> initial 2D array
+
+            dp1 = [float('inf')] * (amount + 1) # --> optimized space as we need only the previous row while evaluating the current row
+
+            dp1[0] = 0
+            
+            for i in range(len(coins)):
+                coinVal = coins[i]
+                curRow = [float('inf')] * (amount+1)
+                for j in range(0, amount + 1):
+                    if j >= coinVal:
+                        curRow[j] = min(dp1[j], curRow[j-coinVal] + 1)
+                    else:
+                        curRow[j] = dp1[j]
+                dp1 = curRow
+            
+            res = dp1[-1]
+
+            return res if res != float('inf') else -1
